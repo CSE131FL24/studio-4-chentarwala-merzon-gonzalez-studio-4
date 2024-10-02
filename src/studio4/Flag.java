d2ea0ba5b3bae52c06cea6b803dd05a45ef563dc
devtarwala <devtarwala@localhost>
2024-10-02 13:50:37 -0500

    e

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,17 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
+		StdDraw.clear();
+		StdDraw.setXscale(10, 15);
+		StdDraw.setYscale(0, 16);
+		StdDraw.setPenColor(40, 20, 40);
+		StdDraw.filledRectangle(3, 3, 3, 3);
+		StdDraw.filledRectangle(3, 13, 3, 3);
+		StdDraw.filledRectangle(16, 3, 6, 3);
+		StdDraw.filledRectangle(16, 13, 6, 3);
+		StdDraw.setPenColor(50, 200, 0);
+		StdDraw.filledRectangle(11, 8, 11, 1);
+		StdDraw.filledRectangle(8, 8, 1, 8);
 		
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,5 +1,6 @@
 package studio4;
 
+import java.awt.Color;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -15,10 +16,53 @@ import edu.princeton.cs.introcs.StdDraw;
 public class InterpretDrawingFile {
 
 	public static void main(String[] args) throws FileNotFoundException {
+		int canvasHeight = 512;
+		int canvasWidth = 22 * canvasHeight / 16;
+		StdDraw.setCanvasSize(canvasWidth, canvasHeight);
+		StdDraw.setXscale(0, 22);
+		StdDraw.setYscale(0, 16);
 		JFileChooser chooser = new JFileChooser("resources");
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
+		String shapeType = in.next();
+		int red = in.nextInt();
+		int green = in.nextInt();
+		int blue = in.nextInt();
+		Color colorofRec = new Color(red,green,blue); 
+		StdDraw.clear();
+		StdDraw.setPenColor(colorofRec);
+		boolean filed = in.nextBoolean();
+		double x = in.nextDouble();
+		double y = in.nextDouble();
+		if (shapeType=="rectangle"){
+			double halfWidth = in.nextDouble();
+			double halfHeight = in.nextDouble();
+			if (filed) {
+				StdDraw.filledRectangle(x, y, halfWidth, halfHeight);
+			}
+			else {
+				StdDraw.rectangle(x, y, halfWidth, halfHeight);
+			}
+		}
+		if (shapeType=="ellipse") {
+			double semiMajor = in.nextDouble();
+			double semiMinor = in.nextDouble();
+			if (filed) {
+				StdDraw.filledEllipse(x, y, semiMajor, semiMinor);
+			}
+			else {
+				StdDraw.ellipse(x, y, semiMajor, semiMinor);
+			}
+		}
+		if (shapeType=="trianlge") {
+			if (filed) {
+				
+			}
+			else {
+				
+			}
+		}
 		
 	}
 }
